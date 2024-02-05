738ff9cff4c93d33c3f7451642a38a24a7336622
Ken Corbett <ken@truepic.com>
2024-02-05 13:52:57 -0500

    Exporting the right api call

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-import type { GetInspectionsResult } from './types/api/Inspections'
+import type { GetInspectionResult } from './types/api/Inspections'
 import type { PopulatedInspection } from './types/composite/Inspections'
 import type { ListItemPayload } from './types/composite/Lists'
 import type { SkinResponseData, AppSettings } from './types/api/Skin'
 
-export type { GetInspectionsResult, PopulatedInspection, ListItemPayload, SkinResponseData, AppSettings }
+export type { GetInspectionResult, PopulatedInspection, ListItemPayload, SkinResponseData, AppSettings }
