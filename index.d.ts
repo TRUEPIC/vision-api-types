01b38147601cbcaaabb43d6128f8c10363819b7b
Ken Corbett <ken@truepic.com>
2024-02-05 13:59:40 -0500

    Importing more types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,16 @@
-import type { GetInspectionResult } from './types/api/Inspections'
-import type { PopulatedInspection } from './types/composite/Inspections'
-import type { ListItemPayload } from './types/composite/Lists'
-import type { SkinResponseData, AppSettings } from './types/api/Skin'
+import type {
+  GetInspectionResult,
+  GetInspectionsResult,
+} from "./types/api/Inspections";
+import type { PopulatedInspection } from "./types/composite/Inspections";
+import type { ListItemPayload } from "./types/composite/Lists";
+import type { SkinResponseData, AppSettings } from "./types/api/Skin";
 
-export type { GetInspectionResult, PopulatedInspection, ListItemPayload, SkinResponseData, AppSettings }
+export type {
+  GetInspectionResult,
+  GetInspectionsResult,
+  PopulatedInspection,
+  ListItemPayload,
+  SkinResponseData,
+  AppSettings,
+};
