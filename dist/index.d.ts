4d255920ae3217d2fd5f3535187c2ca4eade5aca
Ken Corbett <ken@truepic.com>
2024-02-05 12:22:06 -0500

    Cleaning before doing a build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
-import type { GetInspectionsResult } from '@/types/api/Inspections';
-import type { PopulatedInspection } from '@/types/composite/Inspections';
-import type { ListItemPayload } from '@/types/composite/Lists';
-import type { SkinResponseData, AppSettings } from '@/types/api/Skin';
-import { ListType, ListItemType, ListItemQuestionType } from '@/types/enums/List';
+import type { GetInspectionsResult } from './types/api/Inspections';
+import type { PopulatedInspection } from './types/composite/Inspections';
+import type { ListItemPayload } from './types/composite/Lists';
+import type { SkinResponseData, AppSettings } from './types/api/Skin';
+import { ListType, ListItemType, ListItemQuestionType } from './types/enums/List';
 export type { GetInspectionsResult, PopulatedInspection, ListItemPayload, SkinResponseData, AppSettings };
 export { ListType, ListItemType, ListItemQuestionType };
